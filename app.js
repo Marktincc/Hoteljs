578612df43f367e76243f5258800e724f84b828b
Marktin <marktincc@gmail.com>
2023-09-12 15:40:55 -0500

    V1

---
+++ app.js
@@ -0,0 +1,55 @@
+require('colors');
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+
+const {
+  inquirerMenu,
+  pausa,
+  leerInput,
+  crearReserva,
+  verListaReservas,
+  registrarCheckIn,
+  registrarCheckOut,
+  verHabitacionesDisponibles,
+  gestionarServiciosParaHabitacion
+} = require('./helpers/inquirer')
+
+const main = async () => {
+  let opt = '';
+  // const reservas = [];
+  const reservas = leerDB('data.json');
+
+  do {
+      opt = await inquirerMenu();
+
+      switch (opt) {
+          case '1':
+              await crearReserva(reservas);
+              break;
+
+          case '2':
+              verListaReservas(reservas);
+              await pausa();
+              break;
+
+          case '3':
+              await registrarCheckIn(reservas);
+              break;
+
+          case '4':
+              await registrarCheckOut(reservas);
+              break;
+
+          case '5':
+              verHabitacionesDisponibles(reservas);
+              await pausa();
+              break;
+
+          case '6':
+              await gestionarServiciosParaHabitacion(reservas);
+              break;
+      }
+      guardarDB(reservas);
+  } while (opt !== '0');
+}
+
+main();
